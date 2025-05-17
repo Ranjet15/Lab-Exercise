2893696c4b9f936df71bc52c5371b13d59dfa7a3
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-05-17 20:32:34 +0800

    new

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,43 @@
+import { useRouter } from 'next/router';
+
 export default function About() {
-return (
-<div>
-<center>
-<h1>About Page</h1>
+  const router = useRouter();
 
-<p>This page is all about our Next.js project.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-<p>Amet consectetur adipiscing elit quisque faucibus ex sapien.</p>
-<p>Quisque faucibus ex sapien vitae pellentesque sem placerat.</p>
-<p>Vitae pellentesque sem placerat in id cursus mi.</p>
-</center>
-</div>
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '2rem',
+        textAlign: 'center',
+      }}
+    >
+      {/* Top Section with Text */}
+      <div>
+        <h1>About Page</h1>
+        <p>This page is all about our Next.js project.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+        <p>Amet consectetur adipiscing elit quisque faucibus ex sapien.</p>
+        <p>Quisque faucibus ex sapien vitae pellentesque sem placerat.</p>
+        <p>Vitae pellentesque sem placerat in id cursus mi.</p>
+      </div>
 
-
-);
+      {/* Bottom Centered Button */}
+      <div style={{ marginBottom: '2rem' }}>
+        <button
+          onClick={() => router.push('/')}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            cursor: 'pointer',
+          }}
+        >
+          Go to Home
+        </button>
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
+import Link from 'next/link';
+
 export default function Home() {
-return (
-<div>
-<h1>Welcome to My Next.js App!</h1>
-<p>This is the homepage customized by [Your Name].</p>
-</div>
-);
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: '1rem',
+      }}
+    >
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is the homepage customized by [Your Name].</p>
+      <Link href="/about">
+        <button
+          style={{
+            marginTop: '1.5rem',
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            cursor: 'pointer',
+          }}
+        >
+          About Page
+        </button>
+      </Link>
+    </div>
+  );
 }
