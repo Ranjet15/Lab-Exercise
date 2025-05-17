7e0a8b3c5f371ebe8acb7523b451ec4c9c9f3e07
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-05-17 20:19:46 +0800

    lab

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+export default function About() {
+return (
+<div>
+<center>
+<h1>About Page</h1>
+
+<p>This page is all about our Next.js project.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+<p>Amet consectetur adipiscing elit quisque faucibus ex sapien.</p>
+<p>Quisque faucibus ex sapien vitae pellentesque sem placerat.</p>
+<p>Vitae pellentesque sem placerat in id cursus mi.</p>
+</center>
+</div>
+
+
+);
+}
